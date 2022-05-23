4b4d9927f21ded7e2b0f7954fb9af89d3d25a23d
arburhan <buthan190629@outlook.com>
2022-05-23 16:58:27 +0600

    create tools api

--- index.js
+++ index.js
@@ -13,7 +13,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// api
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('Power_Tools').collection('Tools');
 
+        // get all api
+        app.get('/tools', async (req, res) => {
+            const tools = await toolsCollection.find().toArray();
+            res.send(tools);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
