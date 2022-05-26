5a65c13b7cbe475169fd766519a6982dd4f7ed2c
arburhan <buthan190629@outlook.com>
2022-05-26 19:59:08 +0600

    delete console log

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ async function run() {
         // get specific user 
         app.get('/user/:email', async (req, res) => {
             const email = req.params.email;
-            console.log(email)
             const query = { email: email }
             const user = await usersCollection.findOne(query);
             res.send(user);
